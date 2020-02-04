3f9366906df5d6e79338f669c19c2f334592a9c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-04 13:27:44 +0000

    Deploy Muhammadrabi1999/tele-two to github.com/Muhammadrabi1999/tele-two.git:gh-pages

---
+++ precache-manifest.a5ae5a2ebbe67c83f1ac33651cb55137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d42b5de8a3686651d337503e8c7dd3d",
+    "url": "/tele-two/index.html"
+  },
+  {
+    "revision": "a559acdb0884b192e634",
+    "url": "/tele-two/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "366476e21d02da7f3b5d",
+    "url": "/tele-two/static/js/2.ef254d23.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tele-two/static/js/2.ef254d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a559acdb0884b192e634",
+    "url": "/tele-two/static/js/main.bafa0d4f.chunk.js"
+  },
+  {
+    "revision": "d201655fe744a3a0ca95",
+    "url": "/tele-two/static/js/runtime-main.e125f8c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tele-two/static/media/logo.5d5d9eef.svg"
+  }
+]);
